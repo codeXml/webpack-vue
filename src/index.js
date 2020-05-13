56535615b71f86c1de8dde6629363ea2961273b2
shilijuan <shilijuan85@163.com>
2020-05-13 17:32:32 +0800

    用bable转换es6代码，解决兼容性问题，打包并复制html

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './css/index.css'
+console.log('我是js文件')
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,53 @@
 const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const config = {
-    mode: 'production', // development | production
-    entry: path.resolve(__dirname, './src/index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "[name].[chunkhash].js"
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
+    return {
+        mode: 'production', // development | production
+        entry: ["babel-polyfill", path.resolve(__dirname, './src/index.js')],
+        output: {
+            path: path.resolve(__dirname, 'dist/static'),
+            filename: "[name].[chunkhash].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }]
+                }, {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "index.html",
+                filename: "./index.html"
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? '[name].js' : '[name].[hash].css',
+                chunkFilename: devMode ? '[id].css' : '[name].[hash].css',
+            })
+        ]
     }
 }
-
-module.exports = config;
